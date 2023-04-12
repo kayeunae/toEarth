1b8afb04d378794c359fa7c02ac7c939f833bbb7
kayeunae <eunae3029@gmail.com>
2023-04-12 19:48:39 +0900

    up

--- src/main/java/com/toEarth/controller/MemberController.java
+++ src/main/java/com/toEarth/controller/MemberController.java
@@ -30,6 +30,9 @@ public class MemberController {
 		return "member/sign";
 	}
 	
+	
+	
+	
 	@PostMapping(value="signup")
 	public String signUp(@Valid MemberDto signDto, BindingResult bindingResult, Model model) {
 		
@@ -47,6 +50,9 @@ public class MemberController {
 		
 		return "redirect:/";
 	}
+	
+	
+	
 	
 	@GetMapping(value="login")
 	public String logIn() {
