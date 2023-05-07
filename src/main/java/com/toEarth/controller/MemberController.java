a5524378e282a9c9693f0c22313f41db3c5735ce
kayeunae <eunae3029@gmail.com>
2023-05-07 23:26:14 +0900

    up

--- src/main/java/com/toEarth/controller/MemberController.java
+++ src/main/java/com/toEarth/controller/MemberController.java
@@ -48,14 +48,23 @@ public class MemberController {
 		return "redirect:/";
 	}
 	
+	
+	
+	
 	@GetMapping(value="login")
 	public String logIn() {
 		return "member/login";
 	}
+	
+	
+	
 	
 	@GetMapping(value="login/error")
 	public String loginError(Model model) {
 		model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
 		return "member/login";
 	}
+	
+	
+	
 }
